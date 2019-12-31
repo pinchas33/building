8fd7527d86fc4be1fd09bb3fa9d636c83f05260f
pinchas33 <rozenbergpinchas@gmail.com>
2019-12-31 15:07:07 +0200

    build a object

---
+++ Assets/script/LookAtPoint.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class LookAtPoint : MonoBehaviour
+{
+    [Tooltip("number of angales")]
+    public int numVertices = 3; 
+    [HideInInspector]
+    public int firstNumVertices = 3; // varuable to chake if was change in "numVertices"
+    [Tooltip("num that created mesh the roof from him, usely enter same num that 'high'")]
+    public int heightOfTheCenterPointOfTheRoof = 3;
+    [Tooltip("height of the object")]
+    public float hight = 3; 
+   // [HideInInspector]
+    public Vector3[] BottomPoints = new Vector3[3]; //array that inclouded the vector 3 botton points of the object
+    [HideInInspector]
+    public Vector3[] highPoints = new Vector3[3]; //array that inclouded the vector 3 high points of the object
+    [HideInInspector]
+    public Vector3 centerPoint = new Vector3(0,0,0); // point that from it created a mesh to roof
+    [HideInInspector]
+    public int numOfVerticesOnRoof = 0;
+    public bool symmetry = true;
+    Mesh mesh;
+    [HideInInspector]
+     public List<Vector3> vertices = new List<Vector3>();
+
+
+    //public List<Vector2> uvs = new List<Vector2>();
+    public void Reset() //this funtion is calles when reseting the script, and reset the array to size defult
+    {
+        BottomPoints = new Vector3[numVertices];
+        highPoints = new Vector3[numVertices];
+    }
+
+
+    public void changeValue(int newSum , int firitSum) // this funtion is chakes if was change in size the array 
+    {
+        if (newSum != firitSum)
+        {
+            firstNumVertices = numVertices;
+            BottomPoints = new Vector3[numVertices];
+            highPoints = new Vector3[numVertices];
+        }
+    }
+
+    public void getMesh1()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+    }
+
+    public void pointDefinition() //this function is called every frame and update the senter point
+    {
+        centerPoint = highPoints[0] + highPoints[highPoints.Length / 2] * 0.5f;
+
+        centerPoint.y = heightOfTheCenterPointOfTheRoof;
+    }
+
+    public void createWall1(Vector3 highR, Vector3 highL, Vector3 bottonR, Vector3 bottonL)
+    {       // this function is adding to list "vertices" a points of the wall, to send them to mesh in other function
+        vertices.Add(highL); vertices.Add(highR); vertices.Add(bottonL); vertices.Add(bottonL); vertices.Add(highR); vertices.Add(bottonR);
+    }
+
+    public void createTriangle(Vector3 center, Vector3 Edge1, Vector3 Edge2)
+    {       // this function is adding to list "vertices" a points of the triangle, to send them to mesh in other function
+        vertices.Add(center); vertices.Add(Edge1); vertices.Add(Edge2);
+        numOfVerticesOnRoof += 3;
+    }
+
+    public void createMesh()
+    {       // this function is creates a new  list "triangles" that contintion a cirial int numbers to mesh and Initializing the mesh 
+        int[] triangles = new int[vertices.Count];
+
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            triangles[i] = i;
+        }
+
+
+        Vector2[] uvs = new Vector2[vertices.Count];
+        //Debug.Log(numOfVerticesOnRoof);
+        for (int i = 0; i < uvs.Length - (numOfVerticesOnRoof); i+=3)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+
+            //have a problem in case that values that verices.x bettwn a wall equals.
+            if (Math.Abs(Math.Abs(vertices[i].x) - Math.Abs(vertices[i + 1].x)) >= (Math.Abs(Math.Abs(vertices[i].z) - Math.Abs(vertices[i + 1].z))))
+            {
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+                uvs[i +1] = new Vector2(vertices[i+1].x, vertices[i+1].y);
+                uvs[i +2] = new Vector2(vertices[i +2].x, vertices[i+2].y);
+
+            }
+            else if (Math.Abs(Math.Abs(vertices[i].x) - Math.Abs(vertices[i + 1].x)) <= (Math.Abs(Math.Abs(vertices[i].z) - Math.Abs(vertices[i + 1].z))))
+            {
+                uvs[i] = new Vector2(vertices[i].z, vertices[i].y);
+                uvs[i + 1] = new Vector2(vertices[i + 1].z, vertices[i + 1].y);
+                uvs[i + 2] = new Vector2(vertices[i + 2].z, vertices[i + 2].y);
+            }
+
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+    }
+}
+
+
+
+//uvs.Length / numVertices - numVertices
+
+
+
+            //if (vertices[i].x == vertices[i + 1].x && vertices[i + 1].x == vertices[i + 2].x)  
+            //{
+            //    uvs[i] = new Vector2(vertices[i].z, vertices[i].y);
+            //    uvs[i + 1] = new Vector2(vertices[i + 1].z, vertices[i + 1].y);
+            //    uvs[i + 2] = new Vector2(vertices[i + 2].z, vertices[i + 2].y);
+         
+
+            //}
+
+
+
+
---
+++ Assets/script/LookAtPointEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(LookAtPoint))]
+[CanEditMultipleObjects]
+public class LookAtPointEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        var t = (target as LookAtPoint);
+
+        t.changeValue( t.numVertices, t.firstNumVertices);
+
+        t.pointDefinition(); // create a center point
+
+        for (int i = 0; i < t.numVertices; i++) // create handles
+        {
+            t.BottomPoints[i] = Handles.PositionHandle(t.BottomPoints[i], Quaternion.identity);
+        }
+
+
+       // t.centerPoint =  Handles.PositionHandle(t.centerPoint, Quaternion.identity);
+
+
+        for (int i = 0; i < t.numVertices -1; i++) // drow lines between hamdles
+        {
+            Handles.DrawLine(t.BottomPoints[i], t.BottomPoints[i +1]);
+        }
+        Handles.DrawLine(t.BottomPoints[0], t.BottomPoints[t.numVertices-1]);
+
+        if (t.symmetry)
+        {
+            for (int i = 0; i < t.numVertices; i++) // equale up and down
+            {
+                t.highPoints[i] = t.BottomPoints[i] + new Vector3(0, t.hight, 0);
+            }
+
+        }
+
+        t.getMesh1();
+
+        for (int i = 0; i < t.numVertices - 1; i++) //  create walls
+        {
+           t.createWall1(t.highPoints[i], t.highPoints[i + 1], t.BottomPoints[i], t.BottomPoints[i + 1]);
+        }
+        t.createWall1(t.highPoints[t.numVertices -1], t.highPoints[0], t.BottomPoints[t.numVertices -1], t.BottomPoints[0]); //  create last wall
+
+        for (int i = 0; i < t.numVertices - 1; i++) //  create triangles
+        {
+            t.createTriangle(t.centerPoint, t.highPoints[i], t.highPoints[i+1]);
+        }
+        t.createTriangle(t.centerPoint, t.highPoints[t.highPoints.Length - 1], t.highPoints[0]); //  create last triangle
+
+        t.createMesh();
+
+        t.vertices.Clear();
+        //t.uvs.Clear();
+        t.numOfVerticesOnRoof = 0;
+    }
+}
+
+   
